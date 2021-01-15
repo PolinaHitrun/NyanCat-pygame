67762849ba4950cf989fc5d5a8c0ab529403bb0c
polina <phitrun0407@gmail.com>
2021-01-15 18:51:11 +0700

    Кот летает и прыгает два раза

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ FPS = 180
 WIDTH = 700
 HEIGHT = 700
 
+score = 0
+
+
+def game_over():
+    pass
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -51,19 +57,6 @@ def start_screen():
         clock.tick(FPS)
 
 
-# class Camera:
-#     # зададим начальный сдвиг камеры
-#     def __init__(self):
-#         self.dx = 0
-#
-#     # сдвинуть объект obj на смещение камеры
-#     def apply(self, obj):
-#         obj.rect.x += self.dx
-#
-#     # позиционировать камеру на объекте target
-#     def update(self, target):
-#         self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-
 class Blocks(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(blocks_group, all_sprites)
@@ -71,7 +64,6 @@ class Blocks(pygame.sprite.Sprite):
         self.image = pygame.Surface((width, 20))
         self.rect = self.image.get_rect().move(pos_x, pos_y)
         pygame.draw.rect(screen, pygame.Color("red"), (pos_x, pos_y, width, 20))
-
 
     def update(self):
         self.rect = self.rect.move(-2, 0)
@@ -93,7 +85,11 @@ class Food(pygame.sprite.Sprite):
         pass
 
     def update(self):
+        global score
         self.rect = self.rect.move(-2, 0)
+        if pygame.sprite.spritecollideany(self, player_group):
+            score += 1
+            self.kill()
 
 
 class NyanCat(pygame.sprite.Sprite):
@@ -106,7 +102,7 @@ class NyanCat(pygame.sprite.Sprite):
         self.rect = self.rect.move(x, y)
         self.num = 0
         self.jump_counter = 0
-        self.score = 0
+        self.jumping = False
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -123,7 +119,9 @@ class NyanCat(pygame.sprite.Sprite):
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.num = 0
             self.image = self.frames[self.cur_frame]
+
         # self.rect = self.rect.move(0, 3)
+
         if pygame.sprite.spritecollideany(self, blocks_group):
             for block in blocks_group:
                 group = pygame.sprite.Group()
@@ -132,10 +130,14 @@ class NyanCat(pygame.sprite.Sprite):
                     # если прыгает на платформу
                     if block.rect.x <= self.rect.x + self.rect.width <= block.rect.x + block.rect.width\
                             and block.rect.y >= self.rect.y + self.rect.height:
-                        pass
+                        self.jump_counter = 0
                     else:
                         pass
                     # это делаем булевой переменной, а потом при движении проверяем ее
+        else:
+            self.rect = self.rect.move(0, 3)
+        if self.jumping:
+            self.rect = self.rect.move(0, -5)
 
 
 
@@ -153,6 +155,15 @@ class Bomb(pygame.sprite.Sprite):
         self.rect = self.rect.move(-2, 0)
 
 
+# class Invisible(pygame.sprite.Sprite):
+#     def __init__(self, pos_x, pos_y):
+#         super().__init__(food_group, all_sprites)
+#         self.image = pygame.Surface([95, 93])
+#         self.rect = self.image.get_rect()
+#         self.mask = pygame.mask.from_surface(self.image)
+#         self.rect.x = cat.rect.x + 95
+#         self.rect.y = cat.rect.y
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -169,24 +180,39 @@ if __name__ == '__main__':
     blocks_group = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
 
-    cat = NyanCat(load_image("nyan cat.png") , 3 , 3 , 100 , 100)
+    pic = pygame.transform.scale(load_image("nyan cat.png"), (567, 279))
+    cat = NyanCat(pic, 3, 3, 30, 100)
+    # kostyl = Invisible(30, 10)
 
     counter = 0
+    jump_counter = 0
     running = True
     while running:
         counter += 1
         fon = pygame.transform.scale(load_image('fon_game.jpg'), (WIDTH, HEIGHT))
         screen.blit(fon, (0, 0))
+
+        if cat.jumping:
+            jump_counter += 1
+        if jump_counter > 100:
+            cat.jumping = False
+            jump_counter = 0
+
         if counter > 20:
             counter = 0
             block = Blocks(WIDTH, random.randint(0, HEIGHT))
             food = Food(WIDTH, random.randint(0, HEIGHT))
             bomb = Bomb(WIDTH, random.randint(0, HEIGHT))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        blocks_group.update()
-        food_group.update()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                if cat.jump_counter < 2:
+                    cat.jumping = True
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                cat.jump_counter += 1
+
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
